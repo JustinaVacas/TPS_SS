52af67d549f8ac16f59d2cef24bef75981589728
justinavacas <vacaslocas123>
2022-08-11 11:17:29 -0300

    grafico de particulas

---
+++ TP1/graphics/main.py
@@ -0,0 +1,55 @@
+from matplotlib import pyplot as plt
+import sys
+
+
+def draw(particles, neighbours, id):
+    count = 0
+    id = int(id)
+
+    for part in particles:
+        plt.plot(part[0], part[1], marker="o", color="grey")
+
+    for part in particles:
+        if id == count:
+            plt.plot(part[0], part[1], marker="o", color="red")
+        else:
+            for n in neighbours:
+                for i in range(len(n)):
+                    if (count == n[i]) & (n[0] == id):
+                        plt.plot(part[0], part[1], marker="o", color="blue")
+        count += 1
+    plt.show()
+
+
+def parseParameters():
+    with open('utils/Dynamic100.txt') as dynamicFile:
+        dynamicLines = dynamicFile.readlines()[1:]
+
+    with open('utils/output.txt') as outputFile:
+        outputLines = outputFile.readlines()
+
+    neighbours = []
+    for line in outputLines:
+        newline = line.strip()
+        split = newline.split("\t ")
+        count = 0
+        array = []
+        for n in split:
+            array.append(int(split[count]))
+            count += 1
+        neighbours.append(array)
+    # print(neighbours)
+
+    particles = []
+    for line in dynamicLines:
+        newline = line.strip()
+        split = newline.split("   ")
+        particles.append([float(split[0]), float(split[1])])
+    # print(particles)
+
+    return particles, neighbours
+
+
+if __name__ == '__main__':
+    particles, neighbours = parseParameters()
+    draw(particles, neighbours, sys.argv[1])
