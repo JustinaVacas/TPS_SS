c2675ac4b5ee7872a392e40cd283124e377166ce
joassaff <jassaff@itba.edu.ar>
2022-08-31 13:38:08 -0300

    archivos y grafico de la densidad

--- TP2/graphics/graphic.py
+++ TP2/graphics/graphic.py
@@ -8,17 +8,17 @@ def draw(orders1, orders2, orders3, orders4, orders5, orders6):
     #plt.style.use('_mpl-gallery')
 
     # make data:
-    x = [0.1, 0.5, 1, 1.5, 2, 2.5]
-    y = [orders5[0], orders4[0], orders2[0], orders3[0], orders1[0], orders6[0]]
-    yerr = [orders5[1], orders4[1], orders2[1], orders3[1], orders1[1], orders6[1]]
+    x = [0.1, 1, 2, 3, 4, 5]
+    y = [orders1[0], orders2[0], orders3[0], orders4[0], orders5[0], orders6[0]]
+    yerr = [orders1[1], orders2[1], orders3[1], orders4[1], orders5[1], orders6[1]]
 
     # plot:
     fig, ax = plt.subplots()
 
     ax.errorbar(x, y, yerr, fmt='o', linewidth=2, capsize=10, color="purple", ecolor='lightgreen', elinewidth=2)
 
-    ax.set(xlim=(0, 3), xticks=np.arange(0, 3, 0.5),
-           ylim=(0.63, 1.01), yticks=np.arange(0.8, 1.1, 0.2))
+    ax.set(xlim=(0, 5.3), xticks=np.arange(0, 5.3, 1),
+           ylim=(0, 1.1), yticks=np.arange(0, 1.1, 0.2))
 
     plt.xlabel('Ruido')
     plt.ylabel('Orden')
--- TP2/src/utils/GeneratorFiles.java
+++ TP2/src/utils/GeneratorFiles.java
@@ -8,14 +8,14 @@ import java.util.Random;
 
 public class GeneratorFiles {
 
-    public static int N = 550;
+    public static int N = 40;
     public static int L = 7;
     public static double n = 1;
     public static double v = 0.03;
 
     public static void main(String[] args) {
         try {
-            FileWriter myWriter = new FileWriter("./dynamic3_5.txt");
+            FileWriter myWriter = new FileWriter("./dynamic3_1.txt");
             myWriter.write("   0\n");
             for(int i=0; i < N; i++){
                 double randomX = ((Math.random() * (L)) + 0);
@@ -23,7 +23,7 @@ public class GeneratorFiles {
                 myWriter.write("   " + randomX + "   " + randomY + "\n");
             }
             myWriter.close();
-            System.out.println("Successfully wrote dynamic3_5.txt");
+            System.out.println("Successfully wrote dynamic3_1.txt");
         } catch (IOException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
@@ -31,7 +31,7 @@ public class GeneratorFiles {
 
         try {
             final Random random = new Random();
-            FileWriter myWriter = new FileWriter("./static3_5.txt");
+            FileWriter myWriter = new FileWriter("./static3_1.txt");
             myWriter.write("   " + N + "\n");
             myWriter.write("   " + L + "\n");
             myWriter.write("   " + n + "\n");
@@ -42,7 +42,7 @@ public class GeneratorFiles {
                 myWriter.write("   " + ratio + "   " + velocity + "   " + angle + "\n");
             }
             myWriter.close();
-            System.out.println("Successfully wrote static3_5.txt");
+            System.out.println("Successfully wrote static3_1.txt");
         } catch (IOException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
@@ -51,12 +51,12 @@ public class GeneratorFiles {
 
     public static void output(List<Double> orders){
         try {
-            FileWriter myWriter = new FileWriter("./orders3_5.txt");
+            FileWriter myWriter = new FileWriter("./orders3_1.txt");
             for (Double order: orders) {
                 myWriter.write(String.valueOf(order)+'\n');
             }
             myWriter.close();
-            System.out.println("Successfully wrote orders3_5.txt");
+            System.out.println("Successfully wrote orders3_1.txt");
         } catch (IOException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
@@ -65,20 +65,18 @@ public class GeneratorFiles {
 
     public static void outputFrames(List<ArrayList<Double>> frames){
         try {
-            FileWriter myWriter = new FileWriter("./frames3_5.txt");
+            FileWriter myWriter = new FileWriter("./frames3_1.txt");
             int frameNum = 0;
             int count = 0;
             for (ArrayList<Double> frame : frames) {
                 if(count == 0){
                     myWriter.write(String.valueOf(frameNum)+'\n');
-                    frameNum++; //frame=1
-                    System.out.println("count: " + count + "frame " + frameNum);
+                    frameNum++;
                     count = 0;
                 }
                 if(count == N){
                     myWriter.write(String.valueOf(frameNum)+'\n');
-                    frameNum++; //frame=1
-                    System.out.println("count: " + count + "frame " + frameNum);
+                    frameNum++;
                     count = 0;
                 }
                 Double d = frame.get(0);
@@ -88,7 +86,7 @@ public class GeneratorFiles {
 
             }
             myWriter.close();
-            System.out.println("Successfully wrote frames3_5.txt.");
+            System.out.println("Successfully wrote frames3_1.txt.");
         } catch (IOException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
